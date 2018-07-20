536bcda5c47065448b249382c26cba6ce7b90823
Fernando <ferdelamad@gmail.com>
2018-07-20 12:42:45 -0700

    "Added Menu and Table component"

---
+++ client/components/Table.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Table.css';
+
+const Table = () => (
+  <div className='table section'>
+    <h1>I am table</h1>
+  </div>
+);
+
+export default Table;
