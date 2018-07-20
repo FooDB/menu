8f234b0687e18351d80b8d177f049b493e20d855
Fernando <ferdelamad@gmail.com>
2018-07-19 21:47:26 -0700

    "Client getting the menus data with Axios"

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Menu extends Component {
 	constructor(props) {
 		super(props)
+	    this.state = {
+          data: []
+	    };
 
+	}
+
+	componentDidMount() {
+      axios.get('/menus')
+        .then(function(response) {
+        	console.log('Axios resp', response.data)
+        })
 	}
 
 	render() {
