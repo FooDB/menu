dea611f6b24e59fcd267270439e059ca3f9a2dda
Fernando <ferdelamad@gmail.com>
2018-07-25 16:29:17 -0700

    "Refactor server side code"

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,11 @@
 const Menu = require('../db/db.js').Menu;
 
 const getAll = (callback) => {
-  Menu.
-  find({}).
-  exec(callback);
+  Menu
+    .find({})
+    .exec(callback);
 };
 
 module.exports = {
-  getAll,
-}
+  getAll
+};
--- server/server.js
+++ server/server.js
@@ -2,26 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3003;
+const bodyParser = require('body-parser');
 
 const getAll = require('./routes.js').getAll;
 
-//Import Body Parser;
-var bodyParser = require('body-parser');
-
-//servinc static files
 app.use(express.static(path.join(__dirname, '../public')));
 
-//use body parser
 app.use(bodyParser.json());
 
 app.get('/menus', (req, res) => {
   getAll((err, results) => {
-  	if (err) {
-  	  console.log('Error on calling getAll', err);
-  	}
-  	res.send(JSON.stringify(results));
-  })
-
+    if (err) {
+      console.log('Error on calling getAll', err);
+    }
+    res.send(JSON.stringify(results));
+  });
 });
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
