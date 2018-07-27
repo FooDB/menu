c91ca54ec0be5aaaeb7d306aee39d5656fd0905d
Fernando <ferdelamad@gmail.com>
2018-07-27 12:31:44 -0700

    "Rendering 2 columns of the menu by testing part of the data from the server"

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import axios from 'axios';
 import styles from './Menu.css';
 import MenuBar from './MenuBar.jsx';
 import Table from './Table.jsx';
+
 
 class Menu extends Component {
   constructor (props) {
@@ -21,21 +23,23 @@ class Menu extends Component {
           data: response.data[5],
           loaded: true
         }, () => {
-          console.log(response.data[5]);
+          console.log(this.state.data);
         });
       });
   }
 
   render () {
     return (
-      <div className="page">
-        <h1> Menu </h1>
-        <MenuBar loaded={this.state.loaded} data={this.state.data} />
-        <Table />
-        <Table />
-      </div>
+        <div className="page">
+          <h1> Menu </h1>
+          <MenuBar loaded={this.state.loaded} data={this.state.data} />
+          {this.state.loaded && <Table loaded={this.state.loaded} menu={this.state.data.foodMenu}/> }
+          {this.state.loaded && <Table loaded={this.state.loaded} menu={this.state.data.foodMenu}/> }
+        </div>
     );
   }
 }
 
 export default Menu;
+ReactDOM.render(<Menu />, document.getElementById('menu'));
+
--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './Table.css';
 
-const Table = () => (
+const Table = (props) => (
   <div className="table section">
-    <h1>I am table</h1>
+    {props.loaded && props.menu[0].menu[0].items.map(plate =>
+      <div>
+        <div className="title name">{plate.name}</div>
+        <div className="title price">{plate.price}</div>
+        <p className="description">{plate.description}</p>
+      </div>
+     )}
   </div>
 );
 
