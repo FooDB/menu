6ff961b438c7fe7be9eaf40d040d52b39b78b732
Fernando <ferdelamad@gmail.com>
2018-08-02 12:21:37 -0700

    Refactored menu bar view code

--- client/components/MenuBar.jsx
+++ client/components/MenuBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './MenuBar.css';
-import Button from './Button.jsx';
+import Button from './Button';
 
 const MenuBar = (props) => {
   const {
@@ -13,9 +13,9 @@ const MenuBar = (props) => {
     <div className="menu">
       <hr />
       {loaded &&
-        data.foodMenu.map((menu, i) => <Button type={'foodMenu'} index={i} name={menu.name} getViewInfo={getViewInfo} />)}
+        data.foodMenu.map((menu, i) => <Button key={i} type={'foodMenu'} index={i} name={menu.name} getViewInfo={getViewInfo} />)}
       {loaded &&
-        data.drinksMenus.map((menu, i) => <Button type={'drinksMenus'} index={i} name={menu.name} getViewInfo={getViewInfo} />)}
+        data.drinksMenus.map((menu, i) => <Button key={i} type={'drinksMenus'} index={i} name={menu.name} getViewInfo={getViewInfo} />)}
       <hr />
     </div>
   );
